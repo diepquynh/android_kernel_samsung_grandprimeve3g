8439556c8effb6b4cf20bc420efbaffcaf542691
Shoaib0597 <Shoaib0595@gmail.com>
2015-11-19 20:43:12 +0530

    Added support for Dynamic FSync

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2088,6 +2088,7 @@ static inline void iterate_bdevs(void (*f)(struct block_device *, void *), void 
 #endif
 extern int sync_filesystem(struct super_block *);
 extern const struct file_operations def_blk_fops;
+extern void sync_filesystems(int wait);
 extern const struct file_operations def_chr_fops;
 extern const struct file_operations bad_sock_fops;
 #ifdef CONFIG_BLOCK
@@ -2697,3 +2698,4 @@ static inline void inode_has_no_xattr(struct inode *inode)
 }
 
 #endif /* _LINUX_FS_H */
+
