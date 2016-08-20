a576dbb28a49418f447799dae86a925941116b8f
Quyen <quyenach1234@gmail.com>
2016-08-20 14:32:01 +0700

    Revert "cpufreq: Introduce new relation for freq selection"
    This reverts commit ab3ddc958f94cadaebb1a806214f416cffad9842.

--- drivers/cpufreq/freq_table.c
+++ drivers/cpufreq/freq_table.c
@@ -104,7 +104,7 @@ int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 		.index = ~0,
 		.frequency = 0,
 	};
-	unsigned int diff, i = 0;
+	unsigned int i;
 
 	pr_debug("request for target %u kHz (relation: %u) for cpu %u\n",
 					target_freq, relation, policy->cpu);
@@ -114,7 +114,6 @@ int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 		suboptimal.frequency = ~0;
 		break;
 	case CPUFREQ_RELATION_L:
-	case CPUFREQ_RELATION_C:
 		optimal.frequency = ~0;
 		break;
 	}
@@ -150,15 +149,6 @@ int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 					suboptimal.frequency = freq;
 					suboptimal.index = i;
 				}
-			}
-			break;
-		case CPUFREQ_RELATION_C:
-			diff = abs(freq - target_freq);
-			if (diff < optimal.frequency ||
-			    (diff == optimal.frequency &&
-			     freq > table[optimal.driver_data].frequency)) {
-				optimal.frequency = diff;
-				optimal.driver_data = i;
 			}
 			break;
 		}
