bedcd52bc5af293139cfcb7eea5f0416d5e2d4d5
Quyen <quyenach1234@gmail.com>
2016-08-27 20:56:11 +0700

    cpufreq: Add lionfish

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -441,6 +441,9 @@ extern struct cpufreq_governor cpufreq_gov_lagfree;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionheart)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
+extern struct cpufreq_governor cpufreq_gov_lionfish;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionfish)
 #endif
 
 
