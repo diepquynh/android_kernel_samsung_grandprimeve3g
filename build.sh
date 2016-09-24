77fce0f6815ea377ee94404fe184cb478f5140c6
Quyen <quyenach1234@gmail.com>
2016-09-24 13:55:20 +0700

    build.sh: CORE v2.1

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -e -o pipefail
 PLATFORM=sc8830
 DEFCONFIG=grandprimeve3g-dt_defconfig
 NAME=CORE_kernel
-VERSION=v2.0
+VERSION=v2.1
 
 export CROSS_COMPILE=/home/a1/toolchain/sabermod-6.0/bin/arm-eabi-
 export ARCH=arm
