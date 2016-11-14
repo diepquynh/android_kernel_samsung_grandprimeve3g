f2805deb636ad80baa7fe9306774597780225e1f
Quyen <quyenach1234@gmail.com>
2016-11-14 22:36:18 +0700

    [LOCAL ONLY] build.sh: Correct home path

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ DEFCONFIG=grandprimeve3g-dt_defconfig
 NAME=CORE_kernel
 VERSION=v2.3
 
-export CROSS_COMPILE=/home/a1/toolchain/sabermod-6.0/bin/arm-eabi-
+export CROSS_COMPILE=/home/Remilia/toolchain/sabermod-6.0/bin/arm-eabi-
 export ARCH=arm
 export LOCALVERSION=-`echo ${VERSION}`
 
