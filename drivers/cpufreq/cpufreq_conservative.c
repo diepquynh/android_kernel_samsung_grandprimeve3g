563de5611705b086ac7e6ba3600917d0c76b5860
Remilia Scarlet <remilia.1505@gmail.com>
2017-02-21 14:25:13 +0700

    Revert "cpufreq: conservative tweaks"
    This reverts commit 5f0f355b8940036f198e2e36f4898dc2257986ba.

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -27,9 +27,9 @@
 #include "cpufreq_governor.h"
 
 /* Conservative governor macros */
-#define DEF_FREQUENCY_UP_THRESHOLD		(99)
-#define DEF_FREQUENCY_DOWN_THRESHOLD		(96)
-#define DEF_FREQUENCY_STEP			(2)
+#define DEF_FREQUENCY_UP_THRESHOLD		(80)
+#define DEF_FREQUENCY_DOWN_THRESHOLD		(20)
+#define DEF_FREQUENCY_STEP			(5)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
 #define MAX_SAMPLING_DOWN_FACTOR		(10)
 
