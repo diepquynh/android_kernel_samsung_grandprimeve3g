a3571af6ce980ad2a06790a9b3e994c16a02fe26
sultanxda <sultanxda@gmail.com>
2017-05-02 10:16:22 -0700

    mmc: card: Change the I/O sched switcher entry hook point for mmc block devices

--- drivers/mmc/card/queue-sprd.c
+++ drivers/mmc/card/queue-sprd.c
@@ -16,6 +16,7 @@
 #include <linux/kthread.h>
 #include <linux/scatterlist.h>
 #include <linux/version.h>
+#include <linux/iosched_switcher.h>
 
 #include <linux/mmc/card.h>
 #include <linux/mmc/host.h>
@@ -309,6 +310,8 @@ int mmc_init_queue(struct mmc_queue *mq, struct mmc_card *card,
 		ret = PTR_ERR(mq->thread);
 		goto free_bounce_sg;
 	}
+
+	init_iosched_switcher(mq->queue);
 
 	return 0;
  free_bounce_sg:
