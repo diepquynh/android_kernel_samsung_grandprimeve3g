67df39f00a261bde20c00097972b4c3caf0b1a3f
Remilia Scarlet <remilia.1505@gmail.com>
2017-07-16 03:57:02 +0000

    Revert "mm: Update zpool, zswap from Samsung MM kernel"
    This reverts commit 22b278083264583fd8e15896582a7aa18e2f56f6.
    
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- mm/zpool.c
+++ mm/zpool.c
@@ -129,7 +129,6 @@ static void zpool_put_driver(struct zpool_driver *driver)
 /**
  * zpool_create_pool() - Create a new zpool
  * @type	The type of the zpool to create (e.g. zbud, zsmalloc)
- * @name	The name of the zpool (e.g. zram0, zswap)
  * @gfp		The GFP flags to use when allocating the pool.
  * @ops		The optional ops callback.
  *
@@ -141,8 +140,7 @@ static void zpool_put_driver(struct zpool_driver *driver)
  *
  * Returns: New zpool on success, NULL on failure.
  */
-struct zpool *zpool_create_pool(char *type, char *name, gfp_t gfp,
-		struct zpool_ops *ops)
+struct zpool *zpool_create_pool(char *type, gfp_t gfp, struct zpool_ops *ops)
 {
 	struct zpool_driver *driver;
 	struct zpool *zpool;
@@ -170,7 +168,7 @@ struct zpool *zpool_create_pool(char *type, char *name, gfp_t gfp,
 
 	zpool->type = driver->type;
 	zpool->driver = driver;
-	zpool->pool = driver->create(name, gfp, ops);
+	zpool->pool = driver->create(gfp, ops);
 	zpool->ops = ops;
 
 	if (!zpool->pool) {
@@ -345,16 +343,6 @@ void zpool_unmap_handle(struct zpool *zpool, unsigned long handle)
 u64 zpool_get_total_size(struct zpool *zpool)
 {
 	return zpool->driver->total_size(zpool->pool);
-}
-
-unsigned long zpool_compact(struct zpool *zpool)
-{
-	return zpool->driver->compact(zpool->pool);
-}
-
-bool zpool_compactable(struct zpool *zpool, unsigned int pages)
-{
-	return zpool->driver->compactable(zpool->pool, pages);
 }
 
 static int __init init_zpool(void)
